07a97d857509979d6a33534897f5ce0a7109e3f7
DaedalusDev <dev.daedalus@gmail.com>
2019-02-26 08:58:56 +0100

    Exemple

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <nested-component :fn-value="v => v.value"/>
+  </div>
+</template>
+
+<script>
+import NestedComponent from './NestedComponent'
+
+export default {
+  name: 'HelloWorld',
+  components: { NestedComponent },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/NestedComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    A nested component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NestedComponent',
+  props: {
+    fnValue: Function
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
